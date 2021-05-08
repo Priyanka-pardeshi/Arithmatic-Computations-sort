945bdb678023032b0ed2858051ffb57ffa7eadf6
echo # <priyankapardeshi2312@gmail.com>
2021-05-09 00:06:45 +0530

    Added and Forth Expression

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-read -p "enter First nuumber:" a
+read -p "enter First nuumber" a
 read -p "enter Second Number:" b
 read -p "enter Third Number:" c
 
@@ -11,4 +11,8 @@ echo  $result2
 
 result3=`echo $a $b $c | awk '{print $3+$1/$2}'`
 echo $result3
+
+result4=`echo $a $b $c | awk '{print $1%$2+$3}'`
+echo $result4
+
 
