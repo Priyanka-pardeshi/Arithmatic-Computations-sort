a64be05c619a6c9dc55ddc1b454c9b57b8904baf
echo # <priyankapardeshi2312@gmail.com>
2021-05-08 23:59:16 +0530

    Added and second Expression

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -3,5 +3,9 @@ read -p "enter First nuumber:" a
 read -p "enter Second Number:" b
 read -p "enter Third Number:" c
 
-result=`echo $a $b $c | awk '{print $1+$2*$3}'`
-echo $result
+result1=`echo $a $b $c | awk '{print $1+$2*$3}'`
+echo $result1
+
+result2=`echo $a $b $c | awk '{print $1*$2+$3}'`
+echo  $result2
+
