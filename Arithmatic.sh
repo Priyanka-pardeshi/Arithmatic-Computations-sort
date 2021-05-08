cccfcd55431be82d2f88bbf208286c120d2f6079
echo # <priyankapardeshi2312@gmail.com>
2021-05-09 00:11:12 +0530

    Store all results to dict

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,18 +1,29 @@
 #! /bin/bash -x
-read -p "enter First nuumber" a
+
+declare -A expr1
+read -p "enter First nuumber:" a
 read -p "enter Second Number:" b
 read -p "enter Third Number:" c
 
+
 result1=`echo $a $b $c | awk '{print $1+$2*$3}'`
-echo $result1
+
 
 result2=`echo $a $b $c | awk '{print $1*$2+$3}'`
-echo  $result2
+
 
 result3=`echo $a $b $c | awk '{print $3+$1/$2}'`
-echo $result3
+
 
 result4=`echo $a $b $c | awk '{print $1%$2+$3}'`
-echo $result4
+
+
+expr1[results_1]=$result1
+expr1[results_2]=$result2
+expr1[results_3]=$result3
+expr1[results_4]=$result4
+echo ${expr1[@]}
+
+
 
 
